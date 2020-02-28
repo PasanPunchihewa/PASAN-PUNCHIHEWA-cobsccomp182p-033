678bc3878e903cde943eeb65b64a5083dba44102
PasanPunchihewa <PasanPunchihewa@users.noreply.github.com>
2020-02-28 22:59:23 +0530

    Video added and app tested working fine

--- PASAN-PUNCHIHEWA-cobsccomp182p-033/View Controllers/ViewController.swift
+++ PASAN-PUNCHIHEWA-cobsccomp182p-033/View Controllers/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
     
+    var videoPlayer:AVPlayer?
+    
+    var videoPlayerLayer:AVPlayerLayer?
     
     @IBOutlet weak var SignUpBtn: UIButton!
     
@@ -21,9 +25,49 @@ class ViewController: UIViewController {
         setupElements()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up video in the background
+        setUpVideo()
+    }
+    
     func setupElements() {
         Utilities.styleHollowButton(LoginBtn)
         Utilities.styleFilledButton(SignUpBtn)
+    }
+    
+    func setUpVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "NIBM Promo Vid", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x:
+            -self.view.frame.size.width*1.5, y: 0, width:
+            self.view.frame.size.width*4, height:
+            self.view.frame.size.height/2)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 1)
+        
     }
 
 }
