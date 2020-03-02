17ea7c1e1692a6e763776ddd156d85dcb873f3f3
PasanPunchihewa <PasanPunchihewa@users.noreply.github.com>
2020-03-02 18:29:53 +0530

    app icon updated

---
+++ PASAN-PUNCHIHEWA-cobsccomp182p-033/View Controllers/EventHomeViewController.swift
@@ -0,0 +1,62 @@
+//
+//  EventHomeViewController.swift
+//  PASAN-PUNCHIHEWA-cobsccomp182p-033
+//
+//  Created by JOHN DOE on /2/320.
+//  Copyright © 2020 PASAN. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
+
+
+class EventHomeViewController: UIViewController {
+    
+    @IBOutlet weak var TableView: UITableView!
+        var dummyData = ["data 0","data 1","data 2"]
+    @IBOutlet weak var SignOutBtn: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func SignOutClicked(_ sender: Any) {
+        
+        
+        do {
+            try Auth.auth().signOut()
+            let HomeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeVC")
+            
+            self.present(HomeVC, animated: true,completion: nil)
+            transitionToHome()
+            
+        } catch let err {
+            print(err)
+            
+        }
+    }
+    func transitionToHome() {
+        
+        let homeViewController = storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
